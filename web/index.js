94e9a927a6b40d4ab5cf202a864cf6267bf2e6fa
bitevio <110330048+bitevio@users.noreply.github.com>
2023-02-06 13:47:24 +0000

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,6 +1,15 @@
 const {apps,start} =require('bitev.io.app')
 require('dotenv').config()
 
+console.log(
+
+    `
+    Is running with : \n
+    API URL : ${process.env.API_URL}\n
+    API KEY : ${process.env.API_KEY}
+    
+    `
+)
 
 start({
     app:apps.at,

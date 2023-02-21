15e1d21d80ed9c1471c9f7598ee3736538764b8f
bitevio <110330048+bitevio@users.noreply.github.com>
2023-02-21 06:58:50 +0000

    push

---
+++ app/spa/assets/guards.371a9f6a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.b01ba7b0.js";var e=r(async({app:a,router:o,store:t})=>{});export{e as default};
--- server/extensions/api/controls/hooks.controls.js
+++ server/extensions/api/controls/hooks.controls.js
@@ -1,4 +1,11 @@
 //Hooks 
+    const {api} =require("bitev.io/wrappers")
+    
     module.exports ={
+        beforeCreate: ({input})=>{
+
+            console.log(input)
+
+        }
     }
     
